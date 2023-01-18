bf9dc19165ee118e2068c77c6474b20410a5f50e
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-01-18 16:31:22 +0100

    color

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ hexInput.addEventListener(
         if(!isValidHex(hex)) return null
         const strippedHex = hex.replace('#','')
         inputColor.style.background = `#${strippedHex}`
+        // if(convertHexToRGB(hex)) return null
+        // const rgbColor = hex.replace('#', '')
+        // alteredColor.style.background = ${rgbColor}
     }
 )
 
@@ -32,4 +35,5 @@ const convertHexToRGB = (hex) => {
     const r = parseInt(stripped.substring(0,2), 16)
     const g = parseInt(stripped.substring(2,4), 16)
     const b = parseInt(stripped.substring(4,6), 16)
+    return {r,g,b}
 }
